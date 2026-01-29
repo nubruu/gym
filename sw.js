ffbeda515e720913b1b9b8aae249f54f124552b8
nubruuu <nubru7@gmail.com>
2026-01-29 17:09:45 +0530

    Repopulate repository with project files

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'nubru-plan-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './icon-192.png',
+    './icon-512.png',
+    './manifest.json',
+    'https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;700;800&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
