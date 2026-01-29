e62effc54b647210296ec137c1d0c0285311dcc9
nubruuu <nubru7@gmail.com>
2026-01-29 17:46:50 +0530

    Bump SW cache and add update/activate handlers to force new assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nubru-plan-v2';
+const CACHE_NAME = 'nubru-plan-v3';
 const ASSETS = [
     './',
     './index.html',
@@ -11,10 +11,23 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS);
         })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) return caches.delete(key);
+                })
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
